86e7bda2604883e26de4a2809025febdea884f81
FRAZ5094 <frasergmorrison@btinternet.com>
2022-10-18 14:23:49 +0100

    installed assemblyscript to compile wasm

---
+++ assembly/index.ts
@@ -0,0 +1,14 @@
+// The entry file of your WebAssembly module.
+
+export function add(a: i32, b: i32): i32 {
+  return a + b;
+}
+
+export function countToNumber(maxNumber: i32): void {
+  let n : i32 = 0;
+
+  while (n < maxNumber) {
+    n++;
+  }
+
+}
