3746f54df6ebdcfbdc8e85e50aa25e179c45288a
FRAZ5094 <frasergmorrison@btinternet.com>
2022-10-19 21:58:52 +0100

    wrote calculateSpringForce in assemblyscript

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,14 +1,110 @@
 // The entry file of your WebAssembly module.
 
-export function add(a: i32, b: i32): i32 {
-  return a + b;
-}
+export function vectorAdd(v1: StaticArray<f64>, v2: StaticArray<f64>) : StaticArray<f64> {
 
-export function countToNumber(maxNumber: i32): void {
-  let n : i32 = 0;
+  v1[0]+=v2[0];
+  v1[1]+=v2[2];
+  v1[2]+=v2[2];
 
-  while (n < maxNumber) {
-    n++;
-  }
+  return v1;
 
 }
+
+export function vectorSub(v1: StaticArray<f64>, v2: StaticArray<f64>) : StaticArray<f64> {
+
+  v1[0]-=v2[0];
+  v1[1]-=v2[1];
+  v1[2]-=v2[2];
+
+  return v1;
+
+}
+
+export function vectorMultplyScalar(v: StaticArray<f64>, scalar: f64) : StaticArray<f64> {
+
+  v[0]*= scalar;
+  v[1]*= scalar;
+  v[2]*= scalar;
+
+
+  return v;
+
+}
+
+export function vectorDivideScalar(v: StaticArray<f64>, scalar: f64) : StaticArray<f64> {
+
+  return vectorMultplyScalar(v,1/scalar);
+
+}
+
+export function getNormalizedVector(v: StaticArray<f64>) : StaticArray<f64> {
+  
+
+  const mag = getVectorMag(v);
+
+  v[0]/= mag;
+  v[1]/= mag;
+  v[2]/= mag;
+
+  return v;
+
+}
+
+
+export function getVectorMag(v: StaticArray<f64>) : f64 {
+
+  return  Math.sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]);
+
+}
+
+
+export function setVectorMag(v: StaticArray<f64>, newMag: f64) : StaticArray<f64> {
+
+  return vectorMultplyScalar(getNormalizedVector(v), newMag);
+
+}
+
+
+//export const calculateSpringForce = (position1 : THREE.Vector3, position2: THREE.Vector3, k : number, natural_length : number) : THREE.Vector3 => {
+  ////this function takes two positions,k value and natural length and calculates the spring force between those two points on the point position 1
+ 
+  ////position1 is the vertex we want to calculate the spring force on
+  ////position2 is the vertex we imagine the spring is attached to
+
+  ////get distance between the two positions
+  //let dist = position1.sub(position2);
+
+  ////calculate the extension of the spring from the natural length
+  //const extension : number = dist.length() - natural_length;
+
+  ////set the length of the distance vector to the extension of the spring
+  //dist.setLength(extension);
+
+  ////take advantage of the distance vector being in the same direction as the spring force
+  ////f = -kx;
+  //const f = dist.multiplyScalar(-k);
+
+  //return f;
+//}
+
+export function calculateSpringForce(v1: StaticArray<f64>, v2: StaticArray<f64>, k: f64, naturalLength: f64) : StaticArray<f64> {
+
+  console.log(v1.toString());
+  console.log(v2.toString());
+
+  const dist = vectorSub(v1,v2);
+  console.log(dist.toString());
+
+  const extensionMag = getVectorMag(dist) - naturalLength;
+
+  const extensionVector = setVectorMag(dist, extensionMag);
+
+  const f = vectorMultplyScalar(extensionVector,-k);
+
+  return f;
+
+}
+
+
+
+
--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,25 @@
 import assert from "assert";
-import { add } from "../build/debug.js";
-assert.strictEqual(add(1, 2), 3);
-console.log("ok");
+import  * as as  from "../build/debug.js";
+
+//a couple more tests here
+assert.deepEqual(as.vectorAdd([1,1,1],[1,1,1]), [2,2,2]);
+
+assert.deepEqual(as.vectorSub([1,1,1],[1,1,1]), [0,0,0]);
+
+assert.equal(as.getVectorMag([1,0,0]), 1);
+assert.equal(as.getVectorMag([1.02,2.5,0.54]), 2.7535431719876846);
+assert.equal(as.getVectorMag([0,0,0]), 0);
+
+assert.deepEqual(as.getNormalizedVector([5.54, 4.2, 2]), [0.7658218586499821,0.580586968651611,0.2764699850721957]);
+assert.deepEqual(as.getNormalizedVector([1, 0, 0]), [1,0,0]);
+
+assert.deepEqual(as.getNormalizedVector([1, 0, 0]), [1,0,0]);
+
+assert.deepEqual(as.setVectorMag([1,1,1],1), [0.5773502691896258,0.5773502691896258,0.5773502691896258]);
+assert.deepEqual(as.setVectorMag([1,0,0],5), [5,0,0]);
+assert.deepEqual(as.setVectorMag([0,1,0],3), [0,3,0]);
+
+assert.deepEqual(as.getVectorMag(as.calculateSpringForce([12,6,2], [9,-8,2], 23.5, 11)), 77.9687949869943);
+
+console.log("ALL TESTS PASS");
+
