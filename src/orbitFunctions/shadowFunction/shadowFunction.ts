e2928de05b11ea3d9e7c1e131260448872ca369d
FRAZ5094 <frasergmorrison@btinternet.com>
2023-01-14 23:00:43 +0000

    shadow function translated to js

--- src/orbitFunctions/shadowFunction/shadowFunction.ts
+++ src/orbitFunctions/shadowFunction/shadowFunction.ts
@@ -41,3 +41,42 @@ export const isSatelliteInCylindricalUmbra = (
 
   return val <= 0;
 };
+
+export const shadowFunction = (
+  r: number[],
+  sunPos: number[],
+  sunR: number,
+  earthR: number
+): number => {
+  const dx = sunPos[0] - r[0];
+  const dy = sunPos[1] - r[1];
+  const dz = sunPos[2] - r[2];
+
+  const dMag = Math.sqrt(dx * dx + dy * dy + dz * dz);
+
+  const s = Math.sqrt(r[0] * r[0] + r[1] * r[1] + r[2] * r[2]);
+
+  const a = Math.asin(sunR / dMag);
+
+  const b = Math.asin(earthR / s);
+
+  const s_d = dot(-r[0], -r[1], -r[2], dx, dy, dz);
+
+  const c = Math.acos(s_d / (s * dMag));
+
+  const x = (c ** 2 + a ** 2 - b ** 2) / (2 * c);
+
+  if (x >= a) {
+    return 1;
+  }
+
+  const y = Math.sqrt(a ** 2 - x ** 2);
+
+  if (x / a > 1 || x / a < -1) {
+    return 0;
+  }
+
+  const A = a ** 2 * Math.acos(x / a) + b ** 2 * Math.acos((c - x) / b) - c * y;
+
+  return 1 - A / (Math.PI * a ** 2);
+};
